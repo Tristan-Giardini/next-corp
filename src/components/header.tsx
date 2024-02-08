7444b49200d72714bf07c090c05b0ca23143e731
Tristan Giardini <tristangiardini@gmail.com>
2024-02-08 11:14:55 -0500

    Attempt to git init

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <div>
+      <Link href="/">Home</Link>
+      <Link href="/performance">Performance</Link>
+      <Link href="/scale">Scale</Link>
+      <Link href="/reliability">Reliability</Link>
+    </div>
+  )
+}
+
+export default Header
